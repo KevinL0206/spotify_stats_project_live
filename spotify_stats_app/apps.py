f10e5e4be17a999fd804d675192bf2d9f3f4cdf4
Your GitHub Username <k-lam02@outlook.com>
2023-09-14 00:41:36 +0100

    code commit

---
+++ spotify_stats_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class SpotifyStatsAppConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "spotify_stats_app"
---
+++ spotify_stats_project/wsgi.py
@@ -0,0 +1,24 @@
+"""
+WSGI config for spotify_stats_project project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+from whitenoise import WhiteNoise
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "spotify_stats_project.settings")
+
+application = get_wsgi_application()
+
+# Add WhiteNoise middleware to serve static files
+application = WhiteNoise(application, root=os.path.join(os.path.dirname(__file__), 'staticfiles'))
+
+
+app = application
